dea673b449d1f2c575bdd3efce8bbf3c057bab34
Felix Fellman <fellefelixa@gmail.com>
2022-11-10 14:39:37 +0200

    laggt till klass funktioner

---
+++ lektioner/3/message.js
@@ -0,0 +1,8 @@
+const message = () => {
+    const name = "Felix";
+    const age = 19;
+    const loves = ["JavaScript", "Video Games"];
+    return "This is a message from " + name + ". he is " + age + " and he loves " + loves[0] + " and " + loves[1];
+}
+
+export default message;
--- lektioner/3/script.js
+++ lektioner/3/script.js
@@ -21,3 +21,43 @@ function arrayToString(array) {
 
 
 }
+
+class Car {
+    constructor(name, year) {
+      this.name = name;
+      this.year = year;
+    }
+}
+function makeCar() {
+    const name = document.querySelector('#name').value;
+    const year = document.querySelector('#year').value;
+
+    console.log(name, year);
+
+    const myNewCar = new Car(name, year);
+    document.getElementById("carText").innerHTML = "your new car is a " + myNewCar.name + " and was made " + myNewCar.year;
+}
+
+class Cat {
+    constructor(name, color, year) {
+        this.name = name;
+        this.color = color;
+        this.year = year;
+    }
+    age() {
+        let date = new Date();
+        return date.getFullYear() - this.year;
+    }
+}
+function makeCat(){
+    const name = document.querySelector('#catName').value;
+    const color = document.querySelector('#catColor').value;
+    const year = document.querySelector('#catYear').value;
+
+    console.log(name, color, year)
+
+    const cat = new Cat(name, color, year);
+    document.getElementById("catText").innerHTML = "your new cat is called " + cat.name + ". It is " + cat.age() + " years old. It is colored " + cat.color;
+}
+
+
